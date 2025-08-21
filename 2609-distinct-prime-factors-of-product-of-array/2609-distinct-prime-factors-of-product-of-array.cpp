b840424ffdadfcc3c2365ad5716e5ee50baaaa6c
charizard00001 <stillnongdamba@gmail.com>
2025-08-21 17:28:30 +0530

    Time: 7 ms (81.99%), Space: 22.8 MB (47.51%) - LeetHub

--- 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.cpp
+++ 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.cpp
@@ -1,31 +1,21 @@
 class Solution {
+    unordered_map<int, int> M;
 public:
-    vector<int> prime_factors(int N){
-        vector<int> ans;
+    void prime_factors(int N){
         if(N%2 == 0){
-            ans.push_back(2);
-            while(N%2 == 0) N /= 2;
+            M[2]++; while(N%2 == 0) N /= 2;
         }
 
         for(int i = 3 ; i * i <= N ; i+=2){
             if(N%i == 0){
-                ans.push_back(i);
-                while(N%i == 0) N /= i;
+                M[i]++; while(N%i == 0) N /= i;
             }
         }
 
-        if(N > 1) ans.push_back(N); // N is prime ??? why ????
-        return ans;
+        if(N > 1) M[N]++; // N is prime ??? why ????
     }
 
     int distinctPrimeFactors(vector<int>& nums) {
-        unordered_map<int, int> M;
-        for(auto &a : nums){
-            vector<int> factors = prime_factors(a);
-            for(auto &f : factors){
-                M[f]++;
-            }
-        }
-        return M.size();
+        for(auto &a : nums) prime_factors(a); return M.size();
     }
 };
