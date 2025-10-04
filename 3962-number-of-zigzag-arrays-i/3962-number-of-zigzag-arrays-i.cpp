b233e470d37cb122de0d98e4ab3fd49934b16706
charizard00001 <stillnongdamba@gmail.com>
2025-10-04 22:12:48 +0530

    Time: 1986 ms (13.35%), Space: 649.8 MB (25.27%) - LeetHub

--- 3962-number-of-zigzag-arrays-i/3962-number-of-zigzag-arrays-i.cpp
+++ 3962-number-of-zigzag-arrays-i/3962-number-of-zigzag-arrays-i.cpp
@@ -1,50 +1,46 @@
 class Solution {
+    const int MOD = 1e9 + 7;
 public:
+
     int zigZagArrays(int n, int l, int r) {
-        const int MOD = 1e9 + 7;
-        int m = r - l + 1;
+    int m = r - l + 1;
+    vector<vector<vector<int>>> dp(2, vector<vector<int>>(2, vector<int>(r + 2, 0)));
 
-        // dp[dir][x] = number of zigzag arrays ending with value x
-        // dir = 0 → next must go down, dir = 1 → next must go up
-        vector<vector<int>> dp(2, vector<int>(r + 3, 0)), newdp(2, vector<int>(r + 3, 0));
+    // Base case: sequences of length 1
+    for (int x = l; x <= r; x++) {
+        dp[1 % 2][0][x] = 1;  // next down
+        dp[1 % 2][1][x] = 1;  // next up
+    }
 
-        // base case: length 1 arrays
+    for (int i = 1; i < n; i++) {
+        int cur = i % 2, nxt = (i + 1) % 2;
+        fill(dp[nxt][0].begin(), dp[nxt][0].end(), 0);
+        fill(dp[nxt][1].begin(), dp[nxt][1].end(), 0);
+
+        // prefix and suffix sums for optimization
+        vector<int> pre(r + 2, 0), suf(r + 3, 0);
+
+        // For dir = 1 (need up next) → x < y
         for (int x = l; x <= r; x++) {
-            dp[0][x] = 1; // can start going down next
-            dp[1][x] = 1; // or up next
+            pre[x] = (pre[x - 1] + dp[cur][1][x]) % MOD;
+        }
+        for (int y = l; y <= r; y++) {
+            dp[nxt][0][y] = pre[y - 1]; // sum of dp[cur][1][x] for x < y
         }
 
-        // build up to length n
-        for (int len = 2; len <= n; len++) {
-            // reset newdp
-            fill(newdp[0].begin(), newdp[0].end(), 0);
-            fill(newdp[1].begin(), newdp[1].end(), 0);
-
-            // prefix sums for dp[1][x] (for dir=1 → need up next)
-            vector<int> prefix(r + 3, 0);
-            for (int x = l; x <= r; x++)
-                prefix[x] = (prefix[x - 1] + dp[1][x]) % MOD;
-
-            // suffix sums for dp[0][x] (for dir=0 → need down next)
-            vector<int> suffix(r + 4, 0);
-            for (int x = r; x >= l; x--)
-                suffix[x] = (suffix[x + 1] + dp[0][x]) % MOD;
-
-            // transition
-            for (int y = l; y <= r; y++) {
-                // coming from smaller x (x < y)
-                newdp[0][y] = prefix[y - 1];    // next must go down
-                // coming from larger x (x > y)
-                newdp[1][y] = suffix[y + 1];    // next must go up
-            }
-
-            dp.swap(newdp);
+        // For dir = 0 (need down next) → x > y
+        for (int x = r; x >= l; x--) {
+            suf[x] = (suf[x + 1] + dp[cur][0][x]) % MOD;
         }
-
-        long long ans = 0;
-        for (int x = l; x <= r; x++) {
-            ans = (ans + dp[0][x] + dp[1][x]) % MOD;
+        for (int y = l; y <= r; y++) {
+            dp[nxt][1][y] = suf[y + 1]; // sum of dp[cur][0][x] for x > y
         }
-        return ans;
+    }
+
+    long long ans = 0;
+    for (int x = l; x <= r; x++) {
+        ans = (ans + dp[n % 2][0][x] + dp[n % 2][1][x]) % MOD;
+    }
+    return ans;
     }
 };
