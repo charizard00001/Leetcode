3b1766f32815f448deef7e193e3566b62bbcb676
charizard00001 <stillnongdamba@gmail.com>
2025-10-09 23:04:10 +0530

    Time: 7 ms (87.4%), Space: 158.1 MB (85.5%) - LeetHub

---
+++ 4027-number-of-stable-subsequences/4027-number-of-stable-subsequences.cpp
@@ -0,0 +1,38 @@
+const int mod = 1e9 + 7;
+class Solution {
+public:
+    int countStableSubsequences(vector<int>& nums) {
+        vector<int> dp(4, 0);
+        int odd = 0;
+        int even = 0;
+        int n = nums.size();
+        for(int i = 0 ; i < n ; ++i){
+            if(nums[i] & 1){
+                // dp[2] += (odd + dp[1]);
+                dp[2] = (dp[2] + odd) % mod;
+                dp[2] = (dp[2] + dp[1]) % mod;
+                odd++;
+                // dp[1] += (even + dp[0] + dp[3]);
+                dp[1] = (dp[1] + even) % mod;
+                dp[1] = (dp[1] + dp[0]) % mod;
+                dp[1] = (dp[1] + dp[3]) % mod;
+            }
+            else{
+                // dp[3] += (even + dp[0]);
+                dp[3] = (dp[3] + even) % mod;
+                dp[3] = (dp[3] + dp[0]) % mod;
+                even++;
+                // dp[0] += (odd + dp[1] + dp[2]);
+                dp[0] = (dp[0] + odd) % mod;
+                dp[0] = (dp[0] + dp[1]) % mod;
+                dp[0] = (dp[0] + dp[2]) % mod;
+            }
+        }
+        int ans = 0;
+        ans += odd;
+        ans += even;
+        ans %= mod;
+        for(int i = 0 ; i < 4 ; ++i) ans = (ans + dp[i]) % mod;
+        return ans % mod;         
+    }
+};
