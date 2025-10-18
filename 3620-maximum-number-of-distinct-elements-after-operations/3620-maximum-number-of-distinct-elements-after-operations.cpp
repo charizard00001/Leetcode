30352b745fab27bc1f7259e86162180ffc748694
charizard00001 <stillnongdamba@gmail.com>
2025-10-18 11:10:17 +0530

    Time: 133 ms (30.91%), Space: 105.8 MB (28.71%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
-        int ans = 0;
-        int prev = INT_MIN;
-        sort(nums.begin(), nums.end());
-
-        for(auto &a : nums){
-            int minm = a - k;
-            int maxm = a + k;
-
-            if(minm > prev){
-                prev = minm;
-                ans++;
-            }
-            else if(maxm > prev){
-                prev++;
-                ans++;
-            }
+        if(k == 0){
+            set<int> S(nums.begin(), nums.end());
+            return S.size();
         }
-        return ans;
+        else{
+            int minm = INT_MIN;
+            int ans = 0;
+            sort(nums.begin(), nums.end());
+            int n = nums.size();
+            for(int i = 0 ; i < n ; ++i){
+                if(nums[i] - k < minm){
+                    if(minm < nums[i] + k){
+                        minm++;
+                        ans++;
+                    }
+                }
+                else if(nums[i] - k == minm){
+                    minm++;
+                    ans++;
+                }
+                else{
+                    minm = nums[i] - k;
+                    ans++;
+                }
+            }
+            return ans;
+        }
     }
 };
