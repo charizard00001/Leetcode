ac2729fd1fdf32cc179aedb79f95e8c70e1a179a
charizard00001 <stillnongdamba@gmail.com>
2025-10-30 21:06:40 +0530

    Time: 80 ms (54.73%), Space: 18 MB (50%) - LeetHub

--- 3092-minimum-moves-to-spread-stones-over-grid/3092-minimum-moves-to-spread-stones-over-grid.py
+++ 3092-minimum-moves-to-spread-stones-over-grid/3092-minimum-moves-to-spread-stones-over-grid.py
@@ -1,6 +1,12 @@
-from itertools import product, permutations
+# from itertools import product, permutations
+# class Solution:
+#     def minimumMoves(self, g: List[List[int]]) -> int:
+#         z = [(i,j) for i,j in product(*[range(3)]*2) if g[i][j]==0]
+#         q = [(i,j) for i,j in product(*[range(3)]*2) for _ in range(1,g[i][j])]
+#         return min(sum(abs(i-k)+abs(j-l) for (i,j),(k,l) in zip(z,p)) for p in {*permutations(q)})
+
 class Solution:
     def minimumMoves(self, g: List[List[int]]) -> int:
-        z = [(i,j) for i,j in product(*[range(3)]*2) if g[i][j]==0]
-        q = [(i,j) for i,j in product(*[range(3)]*2) for _ in range(1,g[i][j])]
-        return min(sum(abs(i-k)+abs(j-l) for (i,j),(k,l) in zip(z,p)) for p in {*permutations(q)})
+        z = [(i, j) for i, j in product(*[range(3)]*2) if g[i][j] == 0]
+        q = [(i, j) for i, j in product(*[range(3)]*2) for _ in range (1, g[i][j])]
+        return min(sum(abs(i - k) + abs(j - l) for (i, j), (k, l) in zip(z, p)) for p in {*permutations(q)})
